5a4686f964e6343f0d735d628eda0f447b396ab8
cserma <emre.serdengecti@hotmail.com>
2023-09-05 15:51:29 +0300

    snapshot indicator fix, bulkop improvements

--- bulkop/views.py
+++ bulkop/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponse
+from django.utils.translation import gettext_noop as _
+from django.contrib import messages
+from logs.views import addlogmsg
 
 from computes.models import Compute
 from instances.models import Instance
@@ -26,27 +30,54 @@ def bulkop(request):
         request, "bulkoperations.html", {"computes": computes, "instances": instances}
     )
 
-
-
 def bulkStart(request):
     if request.method == 'POST':
         selectedInstances = request.body.decode('utf-8').split(",")
-        
         for selectedInstance in selectedInstances:
             tempInstance = Instance.objects.get(pk=int(selectedInstance))
             if (tempInstance.status != 1):
-                tempInstance.proxy.start()
-        
+                if tempInstance.is_template:
+                    messages.warning(request, _("Templates cannot be started."))
+                else:
+                    tempInstance.proxy.start()
+                    addlogmsg(
+                        request.user.username, tempInstance.compute.name, tempInstance.name, _("Power On")
+                    )
         return JsonResponse({'success': True})  
     
-
 def bulkForceOff(request):
     if request.method == 'POST':
         selectedInstances = request.body.decode('utf-8').split(",")
-        
         for selectedInstance in selectedInstances:
             tempInstance = Instance.objects.get(pk=int(selectedInstance))
-            if (tempInstance.status != 5):
-                tempInstance.proxy.force_shutdown()
-        
+            if (tempInstance.status != 1):
+                if tempInstance.is_template:
+                    messages.warning(request, _("Templates cannot be shutdown."))
+                else:
+                    tempInstance.proxy.force_shutdown()
+                    addlogmsg(
+                        request.user.username, tempInstance.compute.name, tempInstance.name, _("Force Off")
+                    )
         return JsonResponse({'success': True}) 
+    
+def poweron(request, pk):
+    instance = Instance.objects.get(pk=int(pk))
+    if instance.is_template:
+        messages.warning(request, _("Templates cannot be started."))
+    else:
+        instance.proxy.start()
+        addlogmsg(
+            request.user.username, instance.compute.name, instance.name, _("Power On")
+        )
+        return HttpResponse('') 
+
+def forceoff(request, pk):
+    instance = Instance.objects.get(pk=int(pk))
+    if instance.is_template:
+        messages.warning(request, _("Templates cannot be shutdown."))
+    else:
+        instance.proxy.force_shutdown()
+        addlogmsg(
+            request.user.username, instance.compute.name, instance.name, _("Force Off")
+        )
+        return HttpResponse('') 
