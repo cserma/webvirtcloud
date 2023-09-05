9dad705c3127cbc2668c6f8a9ea9c8c6e534918c
cserma <emre.serdengecti@hotmail.com>
2023-09-05 15:51:29 +0300

    snapshot indicator fix, bulkop improvements

--- bulkop/urls.py
+++ bulkop/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "bulkop"
+
 urlpatterns = [
     path("", views.bulkop, name="bulkop"),
     path("bulkStart/", views.bulkStart, name="bulkStart"),
     path("bulkForceOff/", views.bulkForceOff, name="bulkForceOff"),
+    path("poweron/<int:pk>/", views.poweron, name="poweron"),
+    path("forceoff/<int:pk>/", views.forceoff, name="forceoff"),
 ]
